76090e8b96fb61efad764d7d3265d63331ed7a49
Jerry Hsu <Jerry>
2021-09-20 20:35:42 -0400

    Part 1, refactored with useEffect

--- click-to-keep-drawing/src/App.js
+++ click-to-keep-drawing/src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
-import CardPile from './CardPile';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CardPile from "./CardPile";
+
+import "./App.css";
 
 function App() {
+  const [deckId, setDeckId] = useState(null);
+
+  useEffect(() => {
+    async function getDeckId() {
+      const url =
+        "http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+      const resp = await axios.get(url);
+      return resp.data.deck_id;
+    }
+
+    getDeckId().then((id) => setDeckId(id));
+  }, []);
+
   return (
     <div className="App">
-      <CardPile />
+      <CardPile deckId={deckId} />
     </div>
   );
 }
--- click-to-keep-drawing/src/CardPile.js
+++ click-to-keep-drawing/src/CardPile.js
@@ -3,40 +3,33 @@ import axios from "axios";
 import Card from "./Card";
 // import "./CardPile.css";
 
-const CardPile = ()=> {
-  const [ deckId, setDeckId ] = useState(null);
-  const [ cards, setCards ] = useState([]);
+const CardPile = ({ deckId }) => {
+  const [cards, setCards] = useState([]);
+  const [newCard, setNewCard] = useState(false);
 
-  useEffect( ()=>{
-    async function getDeckId() {
-      const url = "http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
-      const resp = await axios.get(url);
-      return resp.data.deck_id;
-    }
-
-    getDeckId().then( id => setDeckId(id) );
-  }
-  , []);
-
-  function getNewCard() {
-    async function queryNewCard() {
+  useEffect(() => {
+    async function getNewCard() {
       const url = `http://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`;
       const resp = await axios.get(url);
       return resp.data.cards[0];
     }
 
-    queryNewCard().then( card => {
-      setCards( cards => [...cards, card]);
-    })
-  }
+    if (deckId) {
+      getNewCard().then((card) => {
+        setCards((cards) => [...cards, card]);
+      });
+    }
+  }, [newCard]);
 
-  return(
+  return (
     <div className="CardPile">
       <h3>Draw a card!</h3>
-      <button onClick={ getNewCard }>Draw card</button>
-      { cards.map( card => <Card card={card}/>)}
+      <button onClick={() => setNewCard(!newCard)}>Draw card</button>
+      {cards.map((card) => (
+        <Card card={card} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default CardPile;
